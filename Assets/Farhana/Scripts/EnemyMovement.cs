5aa65ea726c24319bd36b56c186a9d717109d9b0
farhanaa-noor <farhanaa.noor@gmail.com>
2023-05-07 21:32:19 -0400

    Updated with lfs character

--- Assets/Farhana/Scripts/EnemyMovement.cs
+++ Assets/Farhana/Scripts/EnemyMovement.cs
@@ -4,13 +4,15 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     public Transform target; // the target position for the character to move towards
-    public float attackRange = 2.0f; // the range at which the enemy will start its attack animation
+    public float attackRange = 2.5f; // the range at which the enemy will start its attack animation
     NavMeshAgent agent;
+    public Animator animator;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = agent.GetComponent<Animator>();
     }
 
     void Update()
@@ -22,13 +24,22 @@ public class EnemyMovement : MonoBehaviour
             // set the NavMesh agent's destination to the closest point on the NavMesh surface
             agent.SetDestination(hit.position);
 
+            // set the "Speed" parameter of the Animator based on the NavMeshAgent's velocity
+            animator.SetFloat("Speed", agent.velocity.magnitude);
+
             // check if the player is within attack range
             float distance = Vector3.Distance(transform.position, target.position);
             if (distance < attackRange)
             {
                 // trigger the attack animation
-                GetComponent<Animator>().SetTrigger("Attack");
+                animator.SetTrigger("Attack");
             }
         }
     }
+    public void Die()
+    {
+        animator.SetTrigger("Die");
+        agent.enabled = false;
+    }
+
 }
